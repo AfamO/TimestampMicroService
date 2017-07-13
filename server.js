82891b1542dec547a5d3fe2e81d0435fdcdf5918
System <none>
2017-07-13 10:37:40 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -38,12 +38,17 @@ app.get("/*", function (request, response) {
   var pathParameter=path.replace(re," ");
   if(isNaN(pathParameter)===false)
     {
-      
-       pathParameter=Number(pathParameter);
-      var t = new Date();
-      t.setSeconds( pathParameter);
-      var formattedDate = t.format("dd.mm.yyyy hh:MM:ss");
-      json=getJson(pathParameter,new Date(pathParameter));
+      var unix_timestamp=Number(pathParameter);
+      var date = new Date(unix_timestamp*1000);
+      // Hours part from the timestamp
+      var hours = date.getHours();
+      // Minutes part from the timestamp
+      var minutes = "0" + date.getMinutes();
+      // Seconds part from the timestamp
+      var seconds = "0" + date.getSeconds();
+      // Will display time in 10:30:23 format
+      var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+      json=getJson(pathParameter,formattedTime);
     }
   else
     {
